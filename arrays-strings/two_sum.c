2138bf70c5ff7cd9adbd2b5f18556b908496f804
Chinmayi KC <ugcet2502259@reva.edu.in>
2026-09-20 11:47:37 +0530

    Add local Two Sum test entry point

--- arrays-strings/two_sum.c
+++ arrays-strings/two_sum.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
@@ -17,4 +19,20 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 
     *returnSize = 0;
     return NULL;
+}
+
+int main(void) {
+    int return_size = 0;
+    int numbers[] = {2, 7, 11, 15};
+    int *result = twoSum(numbers, 4, 9, &return_size);
+    assert(result != NULL && return_size == 2);
+    assert(result[0] == 0 && result[1] == 1);
+    free(result);
+
+    int no_solution[] = {1, 2, 3};
+    result = twoSum(no_solution, 3, 10, &return_size);
+    assert(result == NULL && return_size == 0);
+
+    puts("Two Sum: 2 tests passed");
+    return 0;
 }
