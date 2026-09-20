ef19f729fb9c91d0bd4f6590fd5d02e5606e5e82
Chinmayi KC <ugcet2502259@reva.edu.in>
2026-09-20 12:06:54 +0530

    Fix Valid Anagram buffer overflow

--- arrays-strings/valid_anagram.c
+++ arrays-strings/valid_anagram.c
@@ -1,27 +1,26 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 
-int is_anagram(const char *first, const char *second) {
-    if (strlen(first) != strlen(second)) {
-        return 0;
-    }
+bool isAnagram(char *first, char *second) {
     int counts[256] = {0};
-    for (size_t index = 0; first[index] != '\0'; index++) {
+    for (int index = 0; first[index] != '\0'; index++) {
         counts[(unsigned char) first[index]]++;
+    }
+    for (int index = 0; second[index] != '\0'; index++) {
         counts[(unsigned char) second[index]]--;
     }
     for (int index = 0; index < 256; index++) {
         if (counts[index] != 0) {
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
 int main(void) {
-    assert(is_anagram("anagram", "nagaram"));
-    assert(!is_anagram("a", ""));
+    assert(isAnagram("anagram", "nagaram"));
+    assert(!isAnagram("a", ""));
     puts("Valid Anagram: 2 tests passed");
     return 0;
 }
