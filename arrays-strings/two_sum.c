0bea01679e9b094b96d7a5cf9393a60644b5f526
Chinmayi KC <ugcet2502259@reva.edu.in>
2026-09-20 11:46:13 +0530

    Restore requested Two Sum submission code

--- arrays-strings/two_sum.c
+++ arrays-strings/two_sum.c
@@ -1,12 +1,11 @@
-#include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-    int *result = malloc(2 * sizeof(int));
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+     int* result = (int*)malloc(2 * sizeof(int));
 
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
+            
             if (nums[i] + nums[j] == target) {
                 result[0] = i;
                 result[1] = j;
@@ -16,24 +15,6 @@ int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
         }
     }
 
-    free(result);
     *returnSize = 0;
     return NULL;
-}
-
-int main(void) {
-    int return_size = 0;
-    int typical[] = {2, 7, 11, 15};
-    int *result = twoSum(typical, 4, 9, &return_size);
-    assert(return_size == 2);
-    assert(result != NULL && result[0] == 0 && result[1] == 1);
-    free(result);
-
-    int no_solution[] = {1, 2, 3};
-    result = twoSum(no_solution, 3, 10, &return_size);
-    assert(return_size == 0);
-    assert(result == NULL);
-
-    puts("Two Sum: 2 tests passed");
-    return 0;
 }
