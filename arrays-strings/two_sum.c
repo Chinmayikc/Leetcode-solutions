59d08cec06d3a217ac19db41addbf633df3068f0
Chinmayi KC <ugcet2502259@reva.edu.in>
2026-09-20 11:42:15 +0530

    Use LeetCode Two Sum function signature

--- arrays-strings/two_sum.c
+++ arrays-strings/two_sum.c
@@ -1,27 +1,39 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int *two_sum(const int numbers[], int size, int target, int result[2]) {
-    for (int first = 0; first < size; first++) {
-        for (int second = first + 1; second < size; second++) {
-            if (numbers[first] + numbers[second] == target) {
-                result[0] = first;
-                result[1] = second;
+int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
+    int *result = malloc(2 * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
                 return result;
             }
         }
     }
+
+    free(result);
+    *returnSize = 0;
     return NULL;
 }
 
 int main(void) {
-    int result[2];
-    const int typical[] = {2, 7, 11, 15};
-    assert(two_sum(typical, 4, 9, result) != NULL);
-    assert(result[0] == 0 && result[1] == 1);
+    int return_size = 0;
+    int typical[] = {2, 7, 11, 15};
+    int *result = twoSum(typical, 4, 9, &return_size);
+    assert(return_size == 2);
+    assert(result != NULL && result[0] == 0 && result[1] == 1);
+    free(result);
 
-    const int no_solution[] = {1, 2, 3};
-    assert(two_sum(no_solution, 3, 10, result) == NULL);
+    int no_solution[] = {1, 2, 3};
+    result = twoSum(no_solution, 3, 10, &return_size);
+    assert(return_size == 0);
+    assert(result == NULL);
+
     puts("Two Sum: 2 tests passed");
     return 0;
 }
